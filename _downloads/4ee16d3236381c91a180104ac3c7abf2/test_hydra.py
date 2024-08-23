117734b70bd18168ed3e88c67c4dfa396d7a5a49
timkpaine <timkpaine@users.noreply.github.com>
2024-08-23 18:13:13 +0000

    deploy: 316d06bcba8fdcc66bacb07f9ca71152b317d912

--- _downloads/4ee16d3236381c91a180104ac3c7abf2/test_hydra.py
+++ _downloads/4ee16d3236381c91a180104ac3c7abf2/test_hydra.py
@@ -6,12 +6,17 @@ from airflow_supervisor import load_config
 
 
 def test_hydra_config():
-    with patch("airflow_supervisor.config.supervisor.gettempdir") as p1, patch("airflow_supervisor.config.supervisor.datetime") as p2:
+    with (
+        patch("airflow_supervisor.config.supervisor.gettempdir") as p1,
+        patch("airflow_supervisor.config.supervisor.datetime") as p2,
+    ):
         pth = Path(__file__).resolve().parent.parent.parent.parent / ".pytest_cache"
         p1.return_value = str(pth)
         p2.now.return_value = datetime(2000, 1, 1, 0, 0, 0, 1, tzinfo=UTC)
 
-        cfg = load_config("config", overrides=["+program=[sleep,echo]", "+rpcinterface=standard", "+inet_http_server=local"])
+        cfg = load_config(
+            "config", overrides=["+program=[sleep,echo]", "+rpcinterface=standard", "+inet_http_server=local"]
+        )
         assert (
             cfg.to_cfg().strip()
             == """[inet_http_server]
@@ -31,5 +36,7 @@ command=echo
 directory={dir}/echo
 
 [rpcinterface:supervisor]
-supervisor.rpcinterface_factory=supervisor.rpcinterface:make_main_rpcinterface""".format(dir=str(pth / "supervisor-2000-01-01T00:00:00"))
+supervisor.rpcinterface_factory=supervisor.rpcinterface:make_main_rpcinterface""".format(
+                dir=str(pth / "supervisor-2000-01-01T00:00:00")
+            )
         )
