d15580495c7303185e07bf62f1466ca22ff94c6b
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2025-02-14 19:28:59 -0500

    Bump required dependencies

--- airflow_supervisor/tests/config/test_airflow.py
+++ airflow_supervisor/tests/config/test_airflow.py
@@ -1,4 +1,4 @@
-from datetime import UTC, datetime
+from getpass import getuser
 from pathlib import Path
 from unittest.mock import patch
 
@@ -8,11 +8,9 @@ from airflow_supervisor import AirflowConfiguration, ProgramConfiguration, Super
 def test_airflow_inst():
     with (
         patch("supervisor_pydantic.config.supervisor.gettempdir") as p1,
-        patch("supervisor_pydantic.config.supervisor.datetime") as p2,
     ):
         pth = Path(__file__).resolve().parent.parent.parent.parent / ".pytest_cache"
         p1.return_value = str(pth)
-        p2.now.return_value = datetime(2000, 1, 1, 0, 0, 0, 1, tzinfo=UTC)
         c = SupervisorAirflowConfiguration(
             airflow=AirflowConfiguration(port="*:9001"),
             program={
@@ -21,18 +19,16 @@ def test_airflow_inst():
                 )
             },
         )
-        assert str(c.working_dir) == str(pth / "supervisor-2000-01-01T00-00-00")
-        assert str(c.config_path) == str(pth / "supervisor-2000-01-01T00-00-00" / "supervisor.cfg")
+        assert str(c.working_dir) == str(pth / f"supervisor-{getuser()}-test")
+        assert str(c.config_path) == str(pth / f"supervisor-{getuser()}-test" / "supervisor.cfg")
 
 
 def test_airflow_cfg_roundtrip_json():
     with (
         patch("supervisor_pydantic.config.supervisor.gettempdir") as p1,
-        patch("supervisor_pydantic.config.supervisor.datetime") as p2,
     ):
         pth = Path(__file__).resolve().parent.parent.parent.parent / ".pytest_cache"
         p1.return_value = str(pth)
-        p2.now.return_value = datetime(2000, 1, 1, 0, 0, 0, 1, tzinfo=UTC)
         c = SupervisorAirflowConfiguration(
             airflow=AirflowConfiguration(port="*:9001"),
             program={
